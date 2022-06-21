d1f4593ad8a7594a67e3bb2955a7e641f8a42af3
Adrienne <100542795+ampando@users.noreply.github.com>
2022-06-20 20:47:47 -0700

    WIP car.cs.

---
+++ Models/Car.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Dealership.Models {
+
+public class Car
+  {
+    private string _makeModel;
+    private int _price;
+    private int _miles;
+
+    public Car(string makeModel, int price, int miles)
+    {
+      _makeModel = makeModel;
+      _price = price;
+      _miles = miles;
+    }
+    public string GetMakeModel()
+  {
+    return _makeModel;
+  }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (_price <= maxPrice);
+    }
+  }
+}
